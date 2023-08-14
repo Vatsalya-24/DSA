24c7d86bd8299c550ba0485dd7becb017d7c39d8
vatsalya <vatsalyasharma24@gmail.com>
2023-08-14 22:37:00 +0530

    commit

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -10,7 +10,7 @@ class Node{
 };
 Node *head;
 void insert_end(int value){
-    Node *newnode=new Node();\
+    Node *newnode=new Node();
     newnode->data=value;
     if(head==NULL){
         head=newnode;
@@ -19,24 +19,53 @@ void insert_end(int value){
     Node *temp=head;
     while(temp->next!=NULL){
         temp=temp->next;
+        
     }
     temp->next=newnode;
-    
+   
+  
+}
+void insert_beg(int value){
+    Node* newnode= new Node();
+    Node *temp=head;
+    newnode->data=value;
+    if(head==NULL){
+        head=newnode;
+        return;
+    }
+    newnode->next=temp;
+    head=newnode;
+}
+void insert_any_position(int value, int pos){
+    Node* newnode= new Node();
+    Node *temp=head;
+    newnode->data=value;
+    if(head==NULL){
+        head=newnode;
+        return;
+    }
+    for(int i=1;i<pos-1;i++){
+        temp=temp->next;
+    }
+    newnode->next=temp->next;
+    temp->next=newnode;
 }
 void print(){
     Node *temp=head;
-    while(temp->next=NULL){
+    while(temp!=NULL || temp->next!=NULL){
         cout<<temp->data;
+        temp=temp->next;
     }
 }
 int main(){
     Node obj;
     insert_end(2);
     insert_end(3);
+    insert_beg(6);
     insert_end(4);
     insert_end(5);
+    insert_any_position(9,3);
     print();
-    
     
 
 
