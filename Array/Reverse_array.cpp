95c1c76bebae9e0d264d720c61351c7274cac4fe
vatsalya <vatsalyasharma24@gmail.com>
2023-09-03 19:46:41 +0530

    array

--- Array/Reverse_array.cpp
+++ Array/Reverse_array.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
 
+void reverseArray(std::vector<int> &arr, int m) {
+    int n = arr.size();
+    
+    if (m < 0 || m >= n) {
+        std::cout << "Invalid position 'm'." << std::endl;
+        return;
+    }
+
+   
+    for (int i = m, j = n - 1; i < j; i++, j--) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
 int main(){
     
     int arr[5]={1,23,4,5,6};
